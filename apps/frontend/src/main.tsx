0d1e61123a033eba758a62c778ade579fe9b8a34
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-03-29 14:32:07 +0530

    feat: integrate Thirdweb client and provider into frontend

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThirdwebProvider } from "thirdweb/react";
+import App from "./App.tsx";
 import { AuthContextProvider } from "./context/AuthContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <ThirdwebProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </ThirdwebProvider>
   </React.StrictMode>
 );
