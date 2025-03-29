c992d5de5592e5a8131af8b3c8c480df4e032b3e
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-03-30 04:13:34 +0530

    Organized Imports by Accident

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import "./index.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Market from "./components/Card"
-import Register from "./components/Register";
-import { useAuthContext } from "./hooks/useAuthContext";
 import AddProducts from "./components/AddProducts";
+import Market from "./components/Card";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import OffersPage from "./components/OffersPage";
 import ProductPage from "./components/Productpage";
+import Register from "./components/Register";
+import { useAuthContext } from "./hooks/useAuthContext";
+import "./index.css";
 
 function App() {
   const { state } = useAuthContext();
